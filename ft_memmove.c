6bb9fee684d703153d444f39ba0f0b9f7efb14e2
Vincent Masse <vincentmasse3@gmail.com>
2020-11-15 09:13:15 +0100

    protect memcpy + add memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,30 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vmasse <vmasse@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/19 15:21:27 by vmasse            #+#    #+#             */
-/*   Updated: 2020/11/15 08:33:45 by vmasse           ###   ########.fr       */
+/*   Created: 2020/10/20 08:36:50 by vmasse            #+#    #+#             */
+/*   Updated: 2020/11/15 08:33:07 by vmasse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 	char		*dest_buffer;
 	char		*src_buffer;
 
-	if (!dest && !src)
-		return (0);
 	i = -1;
 	dest_buffer = (char *)dest;
 	src_buffer = (char *)src;
-	while (++i < n)
-		dest_buffer[i] = src_buffer[i];
+	if (!dest && !src)
+		return (0);
+	if (src < dest)
+	{
+		while (n-- > 0)
+			dest_buffer[n] = src_buffer[n];
+	}
+	else
+		while (++i < n)
+			dest_buffer[i] = src_buffer[i];
 	dest = (void *)dest_buffer;
 	return (dest);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vmasse <vmasse@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 18:01:20 by vmasse            #+#    #+#             */
-/*   Updated: 2020/11/14 18:10:43 by vmasse           ###   ########.fr       */
+/*   Updated: 2020/11/15 08:01:35 by vmasse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@
 # include <stddef.h>
 # include <stdio.h>
 
-// void  *ft_memmove(void *dest, const void *src, size_t n);
-// void  *ft_memchr(const void *s, int c, size_t n);
 // int	ft_memcmp(const void *s1, const void *s2, size_t n);
 // char  *ft_itoa(int n);
 // char  **ft_strsplit(char const *s, char c);
@@ -29,6 +27,8 @@
 // size_t  ft_strlcpy(char *dst, const char *src, size_t size);
 // size_t  ft_strlcat(char *dst, const char *src, size_t size);
 
+void  *ft_memchr(const void *s, int c, size_t n);
+void  *ft_memmove(void *dest, const void *src, size_t n);
 void  ft_putnbr_fd(int n, int fd);
 void  ft_putendl_fd(char const *s, int fd);
 void  ft_putstr_fd(char const *s, int fd);
