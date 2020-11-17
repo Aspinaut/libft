c9c815aeea4659ff7b5b94f07b993b10d7ee93f3
Vincent Masse <vincentmasse3@gmail.com>
2020-11-17 17:17:32 +0100

    fix strmap

--- ft_strmap.c
+++ ft_strmap.c
@@ -6,7 +6,7 @@
 /*   By: vmasse <vmasse@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 08:20:09 by vmasse            #+#    #+#             */
-/*   Updated: 2020/11/17 13:54:06 by vmasse           ###   ########.fr       */
+/*   Updated: 2020/11/17 17:16:56 by vmasse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,18 @@
 
 char  *ft_strmap(char const *s, char (*f)(char))
 {
-  char *s2;
+  char    *s2;
   size_t  i;
 
   i = 0;
+  if (!f || !s)
+    return (NULL);
   if (!(s2 = ft_strnew(ft_strlen((char *)s))))
     return (NULL);
   while (s[i])
   {
-    (*f)(s2[i++]);
+    s2[i] = (*f)(s[i]);
+    i++;
   }
   s2[i] = '\0';
   return (s2);
